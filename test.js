bcab246bd84442d2baecde7ace73bcb37b802125
emmanuelletallieurfig <eletallieur@lefigaro.fr>
2019-09-27 13:59:24 +0200

    updated test

--- test.js
+++ test.js
@@ -1,47 +1,47 @@
 const LRUCache = require("./LRUCache");
 
-test("LRUCache", async () => {
+test("LRUCache", () => {
   // Crée un LRU cache, avec une capacité max de 2
-  // C'est un cache qui permet
   const cache = new LRUCache(2);
 
-  cache.put(1, "valeurPour1");
-  cache.put(2, "valeurPour2");
-  expect(cache.get(1)).toBe("valeurPour1");
+  cache.put("A", "valeurPourA");
+  cache.put("B", "valeurPourB");
+  expect(cache.get("A")).toBe("valeurPourA");
 
   // On met une troisième valeur, au-delà de la capacité
-  // La clé 2 doit être nettoyée
-  // car la clé 1 a été lue plus récement
-  cache.put(3, "valeurPour3");
+  // La clé B doit être nettoyée
+  // car la clé A a été lue plus récement
+  cache.put("C", "valeurPourC");
   // On vérifie qu'elle n'est plus dans le cache
-  expect(cache.get(2)).toBe(null);
+  expect(cache.get("B")).toBe(null);
 
-  // Cette fois c'est 1 qui va être nettoyée
-  cache.put(4, "valeurPour4");
-  // On ne doit plus avoir que 3 et 4 dans le cache
-  expect(cache.get(1)).toBe(null);
-  expect(cache.get(3)).toBe("valeurPour3");
-  expect(cache.get(4)).toBe("valeurPour4");
+  // Cette fois c'est A qui va être nettoyée
+  cache.put("D", "valeurPourD");
+  // On ne doit plus avoir que C et D dans le cache
+  expect(cache.get("A")).toBe(null);
+  expect(cache.get("B")).toBe(null);
+  expect(cache.get("C")).toBe("valeurPourC");
+  expect(cache.get("D")).toBe("valeurPourD");
 });
 
 test("LRUCache avec capacité 4 ", async () => {
   // On vérifie qu'une capacité plus grande fonctionne aussi
   const cache = new LRUCache(4);
 
-  cache.put(1, "valeurPour1");
-  cache.put(2, "valeurPour2");
-  cache.put(3, "valeurPour3");
-  cache.put(4, "valeurPour4");
+  cache.put("A", "valeurPourA");
+  cache.put("B", "valeurPourB");
+  cache.put("C", "valeurPourC");
+  cache.put("D", "valeurPourD");
 
-  expect(cache.get(3)).toBe("valeurPour3");
-  expect(cache.get(1)).toBe("valeurPour1");
-  // 2 va être nettoyée
-  cache.put(5, "valeurPour5");
-  cache.put(4, "nouvelleValeurPour4");
+  expect(cache.get("C")).toBe("valeurPourC");
+  expect(cache.get("A")).toBe("valeurPourA");
+  // B va être nettoyée
+  cache.put("E", "valeurPourE");
+  cache.put("D", "nouvelleValeurPourD");
 
-  expect(cache.get(1)).toBe("valeurPour1");
-  expect(cache.get(2)).toBe(null);
-  expect(cache.get(3)).toBe("valeurPour3");
-  expect(cache.get(4)).toBe("nouvelleValeurPour4");
-  expect(cache.get(5)).toBe("valeurPour5");
+  expect(cache.get("A")).toBe("valeurPourA");
+  expect(cache.get("B")).toBe(null);
+  expect(cache.get("C")).toBe("valeurPourC");
+  expect(cache.get("D")).toBe("nouvelleValeurPourD");
+  expect(cache.get("E")).toBe("valeurPourE");
 });
